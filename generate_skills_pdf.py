9eba5fd5609fa70f8ff2ed4c7520f389d1cc1d17
Morn2 <aaron13@bluewin.ch>
2024-11-14 22:35:25 +0100

    Coder verändert

--- generate_skills_pdf.py
+++ generate_skills_pdf.py
@@ -19,20 +19,29 @@ def create_document(filename, data):
     width, height = A4
 
     # Überschrift hinzufügen und unterstreichen
-    c.setFont("Helvetica-Bold", 8)  # Schriftgröße halbiert
+    c.setFont("Helvetica-Bold", 13)  # Schriftgröße halbiert
     c.drawString(100, height - 25, "Aaron Feldmann Skill Auflistung")
     c.line(100, height - 27.5, width - 100, height - 27.5)  # Unterstrich hinzufügen
 
     y_position = height - 50
+    column_x_positions = [100, width / 2 + 50]
+    column_index = 0
 
     for category, items in data.items():
-        c.setFont("Helvetica-Bold", 6)  # Schriftgröße halbiert
-        c.drawString(100, y_position, category)
+        c.setFont("Helvetica-Bold", 11)  # Schriftgröße halbiert
+        c.drawString(column_x_positions[column_index], y_position, category)
         y_position -= 10
 
         for name, icon_path in items:
-            c.setFont("Helvetica", 6)  # Schriftgröße halbiert
-            c.drawString(100, y_position, name)
+            if y_position < 40:  # Wenn das Ende der Seite erreicht ist
+                column_index += 1  # Zur nächsten Spalte wechseln
+                y_position = height - 50  # Zurück zum oberen Rand der Seite
+                if column_index >= len(column_x_positions):  # Wenn alle Spalten gefüllt sind
+                    c.showPage()  # Neue Seite beginnen
+                    column_index = 0  # Zurück zur ersten Spalte
+
+            c.setFont("Helvetica", 11)  # Schriftgröße halbiert
+            c.drawString(column_x_positions[column_index], y_position, name)
             y_position -= 10
 
         y_position -= 10  # Leerschlag zwischen Kategorien
@@ -44,5 +53,3 @@ data = read_data_from_csv('daten.csv')
 
 # PDF-Dokument erstellen
 create_document("skills.pdf", data)
-
-
